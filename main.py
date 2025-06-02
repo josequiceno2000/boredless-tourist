4add9ebc6f31206a1755b158bd79d5091f2b9e33
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-06-02 18:42:42 -0500

    Get traveler location and test function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 destinations = [
         "Paris, France",
-        "Shanhai, China",
+        "Shanghai, China",
         "Los Angeles, USA",
         "São Paulo, Brazil",
         "Cairo, Egypt",
@@ -9,6 +9,12 @@ destinations = [
 def get_destination_index(destination: str) -> int:
         """Return the array index of the given destination"""
         return destinations.index(destination)
+
+def get_travaler_location(traveler: str) -> int:
+     """Returns the array index of the traveler's destination"""
+     traveler_destination = traveler[1]
+     return get_destination_index(traveler_destination)
+
 
 def main():
     pass
--- test_main.py
+++ test_main.py
@@ -1,9 +1,8 @@
 import pytest
-from main import get_destination_index
+from main import get_destination_index, get_travaler_location
 
-test_traveler = ["erin Wilkes", "Shanghai, China", ["historical site", "art"]]
 
-class TestClass:
+class TestGetDestinationIndex:
     def test_get_destination_index_one(self):
         destination = "Paris, France"
         assert get_destination_index(destination) == 0
@@ -17,4 +16,15 @@ class TestClass:
         with pytest.raises(ValueError):
             get_destination_index(destination)
 
-
+class TestGetTravelerLocation:
+    def test_get_traveler_location_one(self):
+        traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
+        assert get_travaler_location(traveler) == 1
+    
+    def test_get_traveler_location_two(self):
+        traveler = ["Clint Barton", "Cairo, Egypt", ["historical site", "art"]]
+        assert get_travaler_location(traveler) == 4
+    
+    def test_get_traveler_location_three(self):
+        traveler = ["Sherlock Holmes", "Paris, France", ["historical site", "art"]]
+        assert get_travaler_location(traveler) == 0
